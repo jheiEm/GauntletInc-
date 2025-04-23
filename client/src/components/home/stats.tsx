962d88ad07a8c5232152b4d996498a9d46780fd0
JM <merujeshua@gmail.com>
2025-04-24 04:01:53 +0800

    change stats project to 50

--- client/src/components/home/stats.tsx
+++ client/src/components/home/stats.tsx
@@ -8,7 +8,7 @@ export function Stats() {
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
             <div className="p-6">
-              <Counter end={100} prefix="" suffix="+" />
+              <Counter end={50} prefix="" suffix="+" />
               <p className="text-xl">Projects Completed</p>
             </div>
             <div className="p-6">
